7abdefcee1532f02cb7c15bef789ffb0f2a24168
T9InZ9I <Alex1992tut@mail.ru>
2021-05-19 20:14:57 +0300

    lesson2

---
+++ src/components/app/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Header } from '../header';
+
+import './app.css';
+
+export const App = (props) => (
+    <div className="app">
+        <Header />
+        компонент App
+        </div>
+);
---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './header.css';
+
+export class Header extends React.Component {
+    render() {
+        return(
+            <div className="header">HEADER</div>
+        )
+    }
+}
